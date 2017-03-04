483536aad872414cb94f7567ba1475f3cf37f8d3
Gowtham Goli <sgg308@nyu.edu>
2017-03-04 00:07:06 -0500

    google login added

---
+++ Assets/_Scripts/MainMenu.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine.SocialPlatforms;
+
+public class MainMenu : MonoBehaviour {
+
+    void Awake() {
+        /*PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder()
+            // enables saving game progress.
+            // require access to a player's Google+ social graph (usually not needed)
+            .Build();
+
+        PlayGamesPlatform.InitializeInstance(config);
+        // recommended for debugging:
+        PlayGamesPlatform.DebugLogEnabled = true;
+        // Activate the Google Play Games platform
+        PlayGamesPlatform.Activate();*/
+    }
+    void Start() {
+        // authenticate user:
+        MultiplayerController.Instance.SignInAndStartMPGame();
+        /*Social.localUser.Authenticate((bool success) => {
+            // handle success or failure
+            if (success)
+            {
+                Debug.Log("Sucess login");
+            }
+            else
+            {
+                Debug.Log("Failed");
+            }
+        });*/
+    }
+
+	public void StartMultiPlayer()
+    {
+        Debug.Log("CLicked MP button");
+        MultiplayerController.Instance.SignInAndStartMPGame();
+
+    }
+
+    public void ShowLeaderBoard () {
+        Debug.Log("CLicked Leader button");
+    }
+
+    public void ShowAchievements()
+    {
+        Debug.Log("CLicked Acheive button");
+        PlayGamesPlatform.Instance.SignOut();
+    }
+}
---
+++ Assets/_Scripts/MultiplayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi.Multiplayer;
+
+public class MultiplayerController{
+    private static MultiplayerController _instance = null;
+
+    private MultiplayerController()
+    {
+        PlayGamesPlatform.DebugLogEnabled = true;
+        PlayGamesPlatform.Activate();
+    }
+
+    public void SignInAndStartMPGame()
+    {
+        if (!PlayGamesPlatform.Instance.localUser.authenticated)
+        {
+            PlayGamesPlatform.Instance.localUser.Authenticate((bool success) => {
+                if (success)
+                {
+                    Debug.Log("We're signed in! Welcome " + PlayGamesPlatform.Instance.localUser.userName);
+                    // We could start our game now
+                }
+                else
+                {
+                    Debug.Log("Oh... we're not signed in.");
+                }
+            });
+        }
+        else
+        {
+            Debug.Log("You're already signed in.");
+            // We could also start our game now
+        }
+    }
+
+    public static MultiplayerController Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new MultiplayerController();
+            }
+            return _instance;
+        }
+    }
+}
